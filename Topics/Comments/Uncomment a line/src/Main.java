185f99cf48ba1b1a5aad005bf2c633a46817df91
David Castellanos <davidcastellanos@users.noreply.github.com>
2021-11-26 12:31:30 -0500

    Add files via upload

---
+++ Topics/Comments/Uncomment a line/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * The class implements a simple application with the main method.
+ */
+class CommentedSimpleApplication {
+
+    public static void main(String[] args) {
+        final String i = "893457";
+        System.out.println(i);
+        // System.out.println(789722);
+        // System.out.println(980233);
+    }
+}
